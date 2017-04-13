de4b5c5684670d73600560c72afacdefb545fe3d
PlanetExp <captain.hydd@gmail.com>
2017-04-13 12:50:01 +1000

    added tensorboard plots and comments

--- conv_ca.py
+++ conv_ca.py
@@ -1,21 +1,43 @@
 """Tensorflow model for exploring cellular automaton in convolutional neural
     networks.
 
-This model uses Tensorboard for visualization though various summaries
+The first convolutional layer maps the 3x3 Moore neighborhood input into a
+vector of size "state_size".
+
+input: [batch, with, height, depth]
+input -> state_i
+:
+state_i = state_k
+:
+Optional number of layers k in 0-inf
+state_k -> state_{k+1}
+:
+state_{k+1} -> logits
+
+Basic Architecture:
+conv input (3x3)
+:
+CA module of optional conv layers (3x3) that share weights
+:
+conv output (1x1)
+:
+softmax -> logits
+
+
+This model uses Tensorboard for visualization through various summaries
+Use Tensorboard with "tensorboard --logdir [logdir]"
 
 Author: Frederick Heidrich
 Supervisor: Dr. Frederic Maire
-Date: 
+Date: 13/4/17
 """
 import tensorflow as tf
-import numpy as np
-
-import math
 
 from datetime import datetime
-from time import time, sleep
+from time import time
+import math
 import os
-import re
+# import re
 
 from utils import input_pipeline, maybe_generate_data
 
@@ -25,64 +47,55 @@ class FLAGS(object):
     pass
 
 # Parameters
-FLAGS.state_size = 32
+FLAGS.state_size = 16
 FLAGS.batch_size = 64
+# Number of examples to generate in the training set.
 FLAGS.num_examples = 10000
 FLAGS.num_classes = 2
 # Set number of Cellular Automaton layers to stack.
-FLAGS.num_layers = 6
+FLAGS.num_layers = 12
 # Set whether to reuse variables between CA layers or not.
 FLAGS.reuse = True
 FLAGS.learning_rate = 0.01
 # Number of steps before printing logs.
 FLAGS.log_frequency = 100
 # Set maximum number of steps to train for
-FLAGS.max_steps = 1000
-# Start a number of threads per processor
+FLAGS.max_steps = 10000
+# Start a number of threads per processor.
 FLAGS.num_threads = 4
+# Set various directories for files.
 FLAGS.data_dir = "tmp/data"
 FLAGS.train_dir = "tmp/train"
 FLAGS.valid_dir = "tmp/valid"
+FLAGS.checkpoint_dir = "tmp/train/checkpoints"
+# Whether or not to restore checkpoint of training session.
+# If true, then train for an additional amount of steps in max_steps.
+FLAGS.restore = False
 
 # Data parameters
 width = 9
 height = 9
 depth = 1
 
-# Construct hyperparameter string for our run based on settings
-# (Example: "lr=1e-2,ca=3,state=64")
-hparam_str = "lr=%.e,ca=%d,state=%d" % (FLAGS.learning_rate, FLAGS.num_layers, FLAGS.state_size)
-
-# Generate dataset of (shape) if none exists in data_dir already
-maybe_generate_data(FLAGS.data_dir, shape=(width, height, depth),
-                    num_examples=FLAGS.num_examples)
-
-# Flush train_dir for convenience
-if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-tf.gfile.MakeDirs(FLAGS.train_dir)
-
 
 class ConvCA(object):
     """Defines convolution cellular automaton model"""
-    def __init__(self, inputs, labels, global_step=None, keep_prob=None):
-        self._inputs = inputs
-        self._labels = labels
+    def __init__(self, global_step, keep_prob):
         self._global_step = global_step
         self._keep_prob = keep_prob
 
-        self._create_inference()
-        self._create_loss()
-        self._create_optimizer()
-        self._create_prediction()
+    def __call__(self, inputs, labels):
+        """Creates inference logits and sets up a graph
 
-        # self._create_inference(inputs, keep_prob)
-        # self._create_loss(self._logits, labels)
-        # self._create_optimizer(self._loss, global_step)
-        # self._create_prediction(self._logits, labels)
+        Args:
+            inputs: inputs of shape [batch, width, height, depth]
+            labels: labels of shape [batch]
 
-    def _create_inference(self):
-        """Creates inference logits"""
+        Returns:
+            self: initialized ConvCA object with graph defined
+        """
+        # Helper functions
+        # ===============================================
 
         def _add_summaries(w, b, act):
             """Helper to add summaries"""
@@ -108,10 +121,13 @@ class ConvCA(object):
                 _add_summaries(w, b, act)
             return act
 
+        # Inference
+        # ===============================================
+
         # Input convolution layer
         # -------------------------------------
         # Increase input depth from 1 to state_size
-        conv1 = conv_layer(self._inputs, [3, 3, 1, FLAGS.state_size], name="input_conv")
+        conv1 = conv_layer(inputs, [3, 3, 1, FLAGS.state_size], name="input_conv")
 
         # Cellular Automaton module
         # -------------------------------------
@@ -154,38 +170,43 @@ class ConvCA(object):
                 initializer=tf.constant_initializer(0.0))
             softmax_linear = tf.nn.xw_plus_b(reshape, w, b)
             _add_summaries(w, b, softmax_linear)
+        self._logits = softmax_linear
 
-            self._logits = softmax_linear
+        # Rest of graph
+        # ========================================
 
-    def _create_loss(self):
-        """Create loss function"""
+        # Create loss function
         with tf.name_scope("loss"):
             # Get rid of extra label dimension to [batch_size] and cast to int32
             cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                logits=self._logits, labels=self._labels, name="cross_entropy")
+                logits=self._logits, labels=labels, name="cross_entropy")
             cross_entropy_mean = tf.reduce_mean(cross_entropy)
 
             tf.summary.scalar("loss", cross_entropy_mean)
-            self._loss = cross_entropy_mean
+        self._loss = cross_entropy_mean
 
-    def _create_optimizer(self):
-        """Create training optimizer"""
+        # Create training optimizer
         with tf.name_scope("optimizer"):
             # optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate)
             optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
             minimize = optimizer.minimize(self._loss, global_step=self._global_step)
-            self._optimizer = minimize
+        self._optimizer = minimize
 
-    def _create_prediction(self):
-        with tf.name_scope("prediction"):    
-            correct = tf.equal(tf.argmax(self._logits, 1), tf.cast(self._labels, tf.int64))
+        with tf.name_scope("prediction"):
+            correct = tf.equal(tf.argmax(self._logits, 1), tf.cast(labels, tf.int64))
             # correct = tf.nn.in_top_k(self._logits, self._labels, 1)
             accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
             # print (correct)
             tf.summary.scalar("accuracy", accuracy)
-            self._prediction = accuracy
+        self._prediction = accuracy
+
+        return self  # return complete graph on function call
+
+        # End of graph
+        # =================================================
 
     # Read-only properties
+    # --------------------
     @property
     def inference(self):
         return self._logits
@@ -202,129 +223,164 @@ class ConvCA(object):
     def prediction(self):
         return self._prediction
 
-
-# test that num_classes are correct
-#
-
-# Testing steps
-#
-
-# Get a train data pipeline
-inputs, labels = input_pipeline(FLAGS.data_dir, FLAGS.batch_size,
-                                shape=(width, height, depth),
-                                num_threads=FLAGS.num_threads, train=True)
-
-# Attach an image summary to Tensorboard
-# image_op = tf.summary.image('input', inputs, 3)
-
-# Get validation data pipeline
-# inputs_valid, labels_valid = input_pipeline(FLAGS.batch_size, train=False)
-
-# Keep probability for dropout layer
-keep_prob = tf.placeholder(tf.float32, name="keep_prob")
-
-# Save a global step variable to keep track on steps
-global_step = tf.Variable(0, trainable=False, name="global_step")
-
-# Create model graph
-# model = ConvCA(inputs, labels, global_step, keep_prob)
-model = tf.make_template("train", ConvCA(inputs, labels, global_step, keep_prob))
-test = model(inputs, labels, global_step, keep_prob)
-
-# Initialize all variables that are trainable
-init_op = tf.global_variables_initializer()
-
-# Create up to merge all summaries into one for saving
-merged_summary = tf.summary.merge_all()
-
-# Start Tensorflow session
-with tf.Session() as sess:
-    sess.run(init_op)
-
-    # Create writer to write summaries to file
-    writer = tf.summary.FileWriter(os.path.join(FLAGS.train_dir, hparam_str))
-    writer.add_graph(sess.graph)
-
-    # Create coordinator and start all threads from input_pipeline
-    # The queue will feed our model with data, so no placeholders are necessary
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-   
-    # Save checkpoints for evaluations
-    # saver = tf.train.Saver(max_to_keep=1)
-    
-    # steps_per_epoch = num_examples / batch_size
-    start_time = time()
-    try:
-        # Training loop until coordinator have requested to stop
-        while not coord.should_stop():
-            
-            # Before run
-            # --------------------------------
-            # Train on data for
-            # Take a snapshot of graph stats every 500th step and merge summary
-            # and debug images. Merge summaries every 10th step.
-            feed_dict = {keep_prob: 1.0}
-            sess_run_args = [model.optimizer, model.loss, model.prediction, global_step]
-            _, loss_value, accuracy, step = sess.run(sess_run_args, feed_dict)              
-            
-            if step % 10 == 0:
-                summary = sess.run(merged_summary, feed_dict)
-                writer.add_summary(summary, step)
-            elif step % 500 == 499:
-                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-                run_metadata = tf.RunMetadata()
-                summary = sess.run(merged_summary, feed_dict, run_options, run_metadata)
-                writer.add_run_metadata(run_metadata, "step%d" % step)
-                writer.add_summary(summary, step)
-
-            # After run
-            # --------------------------------
-            # Request to close threads and stop at max_steps
-            if FLAGS.max_steps == step:
-                coord.request_stop()
-
-            if step % FLAGS.log_frequency == 0:
-                current_time = time()
-                duration = current_time - start_time
-                start_time = current_time
-
-                # a = sess.run(model.inference, feed_dict)
-                # print(a[:5])  # debug logits
-
-                # Adjust log freq per time instead!
-                #
-                # sec_per_step = float(duration / step)
-
-                # Save the model for evaluation
-                # save_path = saver.save(sess, FLAGS.train_dir)
-                # print("Model saved in file: %s" % save_path)
-
-                # Restore model for evaluation
-                # saver.restore(sess, FLAGS.train_dir)
-                # print("Model restored.")
-
-                examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-                sec_per_batch = float(duration / FLAGS.log_frequency)
-
-                format_str = ("%s: step %d, loss = %.4f, accuracy = %.3f (%.1f examples/sec; %.3f "
-                    "sec/batch)")
-                print (format_str % (datetime.now(), step, loss_value, accuracy,
-                           examples_per_sec, sec_per_batch))
+    # End of model
+    # =================================================
 
 
-                # train and test accuracies
-                #
+def run_training(run_path):
+    """Run training and validation steps
 
-                # plot
-                #
+    Args:
+        run_path: file path where run data is stored. It is based on
+            hyperparameters.
+    """
+    # Get train and validation data inputs and labels from pipeline
+    inputs, labels = input_pipeline(FLAGS.data_dir, FLAGS.batch_size,
+                                    shape=(width, height, depth),
+                                    num_threads=FLAGS.num_threads, train=True)
+    inputs_valid, labels_valid = input_pipeline(FLAGS.data_dir, FLAGS.batch_size,
+                                                shape=(width, height, depth), 
+                                                num_threads=FLAGS.num_threads, train=False)
+    # Attach an image summary to Tensorboard
+    tf.summary.image('input', inputs_valid, 3)
 
-    except Exception as e:
-        coord.request_stop(e)
-    finally:
-        coord.request_stop()
-        # Wait for threads to finish
-        coord.join(threads)
+    # Keep probability for dropout layer
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
 
-    # Close file writer neatly
-    writer.close()
+    # Save a global step variable to keep track on steps
+    global_step = tf.Variable(0, trainable=False, name="global_step")
+
+    # Create model graph
+    # --------------------------------------------------------------
+    # Here we create two separate graphs that share the same weights
+    # one for validation and one for train, and feed them separate 
+    # input streams. tf.make_template creates a copy of graph with
+    # same variables.
+    model = ConvCA(global_step, keep_prob)
+    template = tf.make_template("model", model)
+    with tf.name_scope("train"):
+        train = template(inputs, labels)
+    with tf.name_scope("valid"): 
+        valid = template(inputs_valid, labels_valid)
+
+    # Initialize all variables that are trainable
+    init_op = tf.global_variables_initializer()
+    # Create op to merge all summaries into one for writing to disk
+    merged_summary = tf.summary.merge_all()
+
+    # Start Tensorflow session
+    with tf.Session() as sess:
+        sess.run(init_op)
+
+        # Create writer to write summaries to file
+        writer = tf.summary.FileWriter(run_path)
+        writer.add_graph(sess.graph)
+
+        # Create coordinator and start all threads from input_pipeline
+        # The queue will feed our model with data, so no placeholders are necessary
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+       
+        # Save checkpoints for evaluations
+        saver = tf.train.Saver(max_to_keep=1)
+        filename = os.path.join(FLAGS.checkpoint_dir, "train.ckpt")
+
+        # Restore model if checkpoint exists
+        step = 0
+        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+        if FLAGS.restore and ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print("Model restored from ", ckpt.model_checkpoint_path)
+            # Take steps from the file path
+            step = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
+            FLAGS.max_steps = FLAGS.max_steps + step
+        else:
+            tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
+
+        start_time = time()
+        try:
+            # Training loop
+            # ==============================================================
+            # Training loop runs until coordinator have got a requested to stop
+            while not coord.should_stop():
+                
+                # Run training
+                # --------------------------------
+                feed_dict = {keep_prob: 1.0}
+                sess_run_args = [train.optimizer, train.loss, train.prediction]
+                _, loss_value, accuracy = sess.run(sess_run_args, feed_dict)
+                step += 1
+
+                # After run and logging
+                # --------------------------------
+                # Take a snapshot of graph stats every 500th step and merge summary
+                # and debug images. Merge summaries every 10th step.
+                if step % 10 == 0:
+                    summary = sess.run(merged_summary, feed_dict)
+                    writer.add_summary(summary, step)
+                elif step % 500 == 499:
+                    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                    run_metadata = tf.RunMetadata()
+                    summary = sess.run(merged_summary, feed_dict, run_options, run_metadata)
+                    writer.add_run_metadata(run_metadata, "step%d" % step)
+                    writer.add_summary(summary, step)
+
+                # Request to close threads and stop at max_steps
+                if FLAGS.max_steps == step:
+                    coord.request_stop()
+
+                if step % FLAGS.log_frequency == 0:
+                    current_time = time()
+                    duration = current_time - start_time
+                    start_time = current_time
+
+                    # print validation accuracy for log (this is also saved in Tensorboard)
+                    valid_accuracy = sess.run(valid.prediction, feed_dict={keep_prob: 1.0})
+                    examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
+                    sec_per_batch = float(duration / FLAGS.log_frequency)
+
+                    format_str = ("%s: step %d, loss = %.4f, accuracy = %.3f (%.3f) "
+                                  "(%.1f examples/sec; %.3f sec/batch)")
+                    print (format_str % (datetime.now(), step, loss_value, accuracy, valid_accuracy,
+                               examples_per_sec, sec_per_batch))
+
+                if step % 500 == 0:
+                     # Save the model once on a while
+                    saver.save(sess, filename, global_step=step)
+
+        except Exception as e:
+            coord.request_stop(e)
+        finally:
+            save_path = saver.save(sess, filename, global_step=step)
+            print("Model saved in file: %s" % save_path)
+
+            coord.request_stop()
+            # Wait for threads to finish
+            coord.join(threads)
+        # Close file writer neatly
+        writer.close()
+
+
+def main(argv=None):
+    # Generate dataset of (shape) if none exists in data_dir already
+    maybe_generate_data(FLAGS.data_dir, shape=(width, height, depth),
+                        num_examples=FLAGS.num_examples)
+
+    # Construct hyperparameter string for our run based on settings
+    # (Example: "lr=1e-2,ca=3,state=64")
+    hparam_str = "lr=%.e,ca=%d,state=%d" % (FLAGS.learning_rate,
+                                            FLAGS.num_layers,
+                                            FLAGS.state_size)
+    run_path = os.path.join(FLAGS.train_dir, hparam_str)
+
+    # Flush run_path for convenience
+    if tf.gfile.Exists(run_path):
+        tf.gfile.DeleteRecursively(run_path)
+    tf.gfile.MakeDirs(run_path)
+
+    run_training(run_path)
+
+
+if __name__ == "__main__":
+    # Runs main with args defined in tf.app.FLAGS
+    tf.app.run()
--- tests.py
+++ tests.py
@@ -5,55 +5,142 @@ import numpy as np
 
 import os
 import utils
+from conv_ca import ConvCA
 
 
 class InputTests(tf.test.TestCase):
 
-	def testReader(self):
-		filepath = os.path.join(self.get_temp_dir(), "test.bin")
+    def testCkpts(self):
+        path = os.path.join(self.get_temp_dir(), "train.ckpt")
+        a = tf.constant([1])
+        b = tf.constant([1])
+        c = tf.Variable(a + b)
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+        with self.test_session() as sess:
+            sess.run(init)
+            for i in range(10):
+                cc = sess.run(c)
+                if i % 5 == 0:
+                    s = saver.save(sess, path, i)
+                    print ("saved in ", s, i)
+            print ("var: ", cc)
 
-		shape = (5, 5, 1)
-		image = np.arange(25, dtype=np.int8).reshape(shape)
-		label = np.ones((1), dtype=np.int8)
-		batch_i = np.reshape(image, (1, 5, 5, 1))
-		batch_l = np.reshape(label, (1, 1))
-		utils._convert_to_tfrecords(batch_i, shape, batch_l, filepath)
+            # ckpt_path = tf.train.latest_checkpoint(self.get_temp_dir())
+            ckpt = tf.train.get_checkpoint_state(self.get_temp_dir())
+            # tf.train.get_checkpoint_path()
+            print (ckpt.model_checkpoint_path)
+            # print (ckpt.get_checkpoint_path)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print("Model restored from ", ckpt.model_checkpoint_path)
+                step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            print (step)
 
-		with self.test_session() as sess:
-			q = tf.FIFOQueue(99, tf.string)
-			q.enqueue([filepath]).run()
-			q.close().run()
-			tf.train.start_queue_runners(sess)
-			i, l = utils.read_and_decode(q, shape)
-			ii, ll = sess.run([i, l])
 
-			self.assertAllEqual(ii, image)
-			self.assertAllEqual(ll, label)
+    # def testTemplateNames(self):
+    #     def var_name(x, name):
+    #         a = tf.get_variable(name,
+    #                             shape=[2, 2],
+    #                             initializer=tf.constant_initializer(1))
+    #         return a
 
-	def testGenerator(self):
-		filepath = os.path.join(self.get_temp_dir(), "test.bin")
-		shape = (20, 20, 1)
-		num_examples = 4
-		x, y = utils.generate_constrained_dataset(None, shape, num_examples, False, 0.5)
-		utils._convert_to_tfrecords(x, shape, y, filepath)
+    #     with tf.name_scope("bar"):
+    #         var_name_foo = tf.make_template("var_name_foo", var_name, name="foo")
+    #         a = var_name_foo(tf.constant([1]))
+    #     with tf.name_scope("bar"):
+    #         b = var_name_foo(tf.constant([0]))
 
-		# print(x[3].reshape((5, 5)), y[3])
+    #     print (a, b)
+        # with self.test_session() as sess:
+        #     sess.run(tf.global_variables_initializer())
+        #     aaa, bbb = sess.run([a, bb])
+        #     self.assertAllEqual(aaa, bbb)
 
-		with self.test_session() as sess:
-			q = tf.FIFOQueue(99, tf.string)
-			q.enqueue([filepath]).run()
-			q.close().run()
-			tf.train.start_queue_runners(sess)
-			xx, yy = utils.read_and_decode(q, shape)
-			yy = tf.squeeze(yy)
+    # def testScope(self):
+    #     def s(name=None):
+    #       with tf.variable_scope("scope") as vs:
+    #           a = tf.get_variable(name, [1, 2, 3], initializer=tf.constant_initializer(1))
+    #           vs.reuse_variables()
+    #           b = tf.get_variable(name, [1, 2, 3], initializer=tf.constant_initializer(0))
+    #       return a, b
+    #     scope_copy = tf.make_template("scope", s)
+    #     a, b = scope_copy("foo")
+    #     aa, bb = scope_copy("foo")
 
-			for i in range(num_examples):
-				image, label = sess.run([xx, yy])
-				# print (image.reshape((5, 5)), label)
-				self.assertAllEqual(x[i], image)
-				self.assertAllEqual(y[i], label)
+    #     with self.test_session() as sess:
+    #         sess.run(tf.global_variables_initializer())
+    #         aaa, bbb = sess.run([a, bb])
+    #         self.assertAllEqual(aaa, bbb)
 
-				# Add tests for correct label and image shape sizes
+    # def testTemplate(self):
+    #     k = tf.placeholder(tf.float32, name="keep_prob")
+    #     g = tf.Variable(0, trainable=False, name="global_step")
+    #     m = ConvCA(g, k)
+    #     it = tf.placeholder(tf.float32, [64, 9, 9, 1])
+    #     iv = tf.placeholder(tf.float32, [64, 9, 9, 1])
+    #     il = tf.placeholder(tf.int64, [64, ])
+    #     t = tf.make_template("model", m)
+    #     # model = t(g, k)
+    #     valid = t(iv, il)
+    #     train = t(it, il)
+
+    #     data1 = np.ones((64, 9, 9, 1))
+    #     data2 = np.zeros((64, 9, 9, 1))
+    #     label = np.ones((64, ))
+
+    #     with self.test_session() as sess:
+    #         sess.run(tf.global_variables_initializer())
+    #         a, b = sess.run([train.prediction, valid.prediction], feed_dict={k: 1.0, it: data1, iv: data2, il: label})
+    #         self.assertEqual(train, valid)
+    #         self.assertNotEqual(a, b)
+
+
+    # def testReader(self):
+    #   filepath = os.path.join(self.get_temp_dir(), "test.bin")
+
+    #   shape = (5, 5, 1)
+    #   image = np.arange(25, dtype=np.int8).reshape(shape)
+    #   label = np.ones((1), dtype=np.int8)
+    #   batch_i = np.reshape(image, (1, 5, 5, 1))
+    #   batch_l = np.reshape(label, (1, 1))
+    #   utils._convert_to_tfrecords(batch_i, shape, batch_l, filepath)
+
+    #   with self.test_session() as sess:
+    #       q = tf.FIFOQueue(99, tf.string)
+    #       q.enqueue([filepath]).run()
+    #       q.close().run()
+    #       tf.train.start_queue_runners(sess)
+    #       i, l = utils.read_and_decode(q, shape)
+    #       ii, ll = sess.run([i, l])
+
+    #       self.assertAllEqual(ii, image)
+    #       self.assertAllEqual(ll, label)
+
+    # def testGenerator(self):
+    #   filepath = os.path.join(self.get_temp_dir(), "test.bin")
+    #   shape = (20, 20, 1)
+    #   num_examples = 4
+    #   x, y = utils.generate_constrained_dataset(None, shape, num_examples, False, 0.5)
+    #   utils._convert_to_tfrecords(x, shape, y, filepath)
+
+    #   # print(x[3].reshape((5, 5)), y[3])
+
+    #   with self.test_session() as sess:
+    #       q = tf.FIFOQueue(99, tf.string)
+    #       q.enqueue([filepath]).run()
+    #       q.close().run()
+    #       tf.train.start_queue_runners(sess)
+    #       xx, yy = utils.read_and_decode(q, shape)
+    #       yy = tf.squeeze(yy)
+
+    #       for i in range(num_examples):
+    #           image, label = sess.run([xx, yy])
+    #           # print (image.reshape((5, 5)), label)
+    #           self.assertAllEqual(x[i], image)
+    #           self.assertAllEqual(y[i], label)
+
+                # Add tests for correct label and image shape sizes
 
 if __name__ == "__main__":
-	tf.test.main()
+    tf.test.main()
